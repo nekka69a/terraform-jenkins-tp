4181e240ba48f2c39b49ff97164d9fe294bfac15
Nekka Amina <123459087+nekka69a@users.noreply.github.com>
2023-06-09 11:19:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  client_id       = "5bfbb9da-9e56-4c19-912d-6cd356dd5c3a"
+  client_secret   = BOv8Q~xuGsnYfnumX6RMkKflIW4aaakXRHhLxcFH
+  subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+  tenant_id       = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
+}
+
 resource "azurerm_resource_group" "exampleamina" {
   name     = "exampleamina-resources"
   location = "West Europe"
